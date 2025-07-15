6479d8ca5589a2afb188f881cd89e089662eace5
Talha Tech <talhatech116@gmail.com>
2025-07-15 12:48:26 +0500

    Brands API

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\BrandController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -12,9 +13,13 @@ use App\Http\Controllers\Admin\CategoryController;
 Route::post('/admin/login' , [AuthController::class, 'authenticate']);
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
-    Route::get('/admin/categories', [CategoryController::class, 'index']);
-    Route::post('/admin/categories', [CategoryController::class, 'store']);
-    Route::get('/admin/categories/{id}', [CategoryController::class, 'show']);
-    Route::put('/admin/categories/{id}', [CategoryController::class, 'update']);
-    Route::delete('/admin/categories/{id}', [CategoryController::class, 'destroy']);
+    // Route::get('/admin/categories', [CategoryController::class, 'index']);
+    // Route::post('/admin/categories', [CategoryController::class, 'store']);
+    // Route::get('/admin/categories/{id}', [CategoryController::class, 'show']);
+    // Route::put('/admin/categories/{id}', [CategoryController::class, 'update']);
+    // Route::delete('/admin/categories/{id}', [CategoryController::class, 'destroy']);
+
+
+    Route::resource('categories', CategoryController::class);
+    Route::resource('brands', BrandController::class);
 });
